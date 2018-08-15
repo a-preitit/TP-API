db1c49d10696f5180db612614ae57d03d1c3da58
preiti tasat axel facundo <43871824@ort>
2018-08-15 15:47:29 -0300

    Ubicacion agregada

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,20 @@
 console.log("Listo!");
+
+var map;
+function initMap() {
+	map = new google.maps.Map(document.getElementById('#map'), {
+  		center: {lat: -34.549411, lng: -58.454080},
+  		zoom: 15
+	});
+}
+
+/*window.onload = getMyLocation;
+function getMyLocation(){
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(displayLocation);
+	} else {
+		initMap();
+	}
+}*/
+
+	      
