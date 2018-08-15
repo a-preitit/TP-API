6b848fac09427f295158c1291aa229dd73c25fd9
preiti tasat axel facundo <43871824@ort>
2018-08-15 15:51:58 -0300

    Ya corre en el JS

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,37 @@
 console.log("Listo!");
 
 var map;
-function initMap() {
-	map = new google.maps.Map(document.getElementById('#map'), {
-  		center: {lat: -34.549411, lng: -58.454080},
-  		zoom: 15
-	});
-}
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.549411, lng: -58.454080},
+      zoom: 15
+    });
+  }
 
-/*window.onload = getMyLocation;
-function getMyLocation(){
-	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(displayLocation);
-	} else {
-		initMap();
-	}
-}*/
+  window.onload = getMyLocation;
+  function getMyLocation(){
+  	if(navigator.geolocation){
+  		navigator.geolocation.getCurrentPosition(displayLocation);
+  	} else {
+  		initMap();
+  	}
+  }
+
+  function displayLocation(position){
+  	var latitude = position.coords.latitude;
+  	var longitude = position.coords.longitude;
+  	var latLng = new google.maps.LatLng(latitude, longitude);
+  	showMap(latLng);
+  }
+
+  function showMap(latLng){
+  	var mapOptions = {
+  		center: latLng,
+  		zoom: 15,
+  		mapTypeId: google.maps.MapTypeId.ROADMAP
+  	};
+
+  	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  }
 
 	      
